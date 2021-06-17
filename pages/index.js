8ae8ae6632c478043fb1ead6c311a92d288a7bba
lkmSasanga <lkmsasanga@gmail.com>
2021-06-17 16:37:30 +0530

    wp posts requested to the nextjs

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,42 @@
-export default function Home() {
-  return <h1> Hello from the Home Page </h1>;
+export default function Home({ posts }) {
+  return (
+    <div>
+      <h1> Hello from the Home Page </h1>
+      {posts.nodes.map((post) => {
+        return (
+          <ul key={post.slug}>
+            <li>{post.title}</li>
+          </ul>
+        );
+      })}
+    </div>
+  );
+}
+
+export async function getStaticProps() {
+  // run query inside here
+  const res = await fetch("http://refurbly.local/graphql", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      query: `
+          query HomePageQuery {
+            posts {
+              nodes {
+                slug
+                title
+              }
+            }
+          }
+        `,
+    }),
+  });
+
+  const json = await res.json();
+
+  return {
+    props: {
+      posts: json.data.posts,
+    },
+  };
 }
